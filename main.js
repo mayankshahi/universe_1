0c76cace8f6d47bf94656aa0884b9a3f5d81365a
mayankshahi <mayank.shahi@gmail.com>
2022-02-10 08:38:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function more_universe()
+{
+    document.getElementById("more_universe").innerHTML = "The earliest cosmological models of the universe were developed by ancient Greek and Indian philosophers and were geocentric, placing Earth at the center. Over the centuries, more precise astronomical observations led Nicolaus Copernicus to develop the heliocentric model with the Sun at the center of the Solar System. In developing the law of universal gravitation, Isaac Newton built upon Copernicus's work as well as Johannes Kepler's laws of planetary motion and observations by Tycho Brahe."
+    document.getElementById("blablabla").innerHTML  =  "  "
+}
